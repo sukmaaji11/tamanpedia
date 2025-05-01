95b6f3c757bf54bcca1e842b85322bd64af57b43
sukmaaji11 <sukmaaji11@gmail.com>
2025-05-01 12:24:34 +0700

    Setting Routes Index Fix

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome_message';
+$route['default_controller'] = 'Welcome';
 
 //Auth
 $route['auth'] = 'C_auth';
