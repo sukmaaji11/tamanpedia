284f2747e37290a56de5a9115ee6b6755803ab15
sukmaaji11 <sukmaaji11@gmail.com>
2025-05-01 12:20:41 +0700

    Setting Base Url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://sys.sasanangapak.com/';
+$config['base_url'] = 'https://tamanpedia.bra-dev.com/';
 
 /*
 |--------------------------------------------------------------------------
