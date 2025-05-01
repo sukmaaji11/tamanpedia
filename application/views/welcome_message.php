8c0fd3e7d7b1271789a663857fb983d7b60e2a17
sukmaaji11 <sukmaaji11@gmail.com>
2025-05-01 12:56:17 +0700

    Add Welcome Message To Landing Page Fix

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -18,14 +18,14 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="<? base_url('assets/template/landing/')?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<? base_url('assets/template/landing/')?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<? base_url('assets/template/landing/')?>assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="<? base_url('assets/template/landing/')?>assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="<? base_url('assets/template/landing/')?>assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="<? base_url('assets/template/landing/assets/vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+  <link href="<? base_url('assets/template/landing/assets/vendor/bootstrap-icons/bootstrap-icons.css')?>" rel="stylesheet">
+  <link href="<? base_url('assets/template/landing/assets/vendor/aos/aos.css')?>" rel="stylesheet">
+  <link href="<? base_url('assets/template/landing/assets/vendor/glightbox/css/glightbox.min.css')?>" rel="stylesheet">
+  <link href="<? base_url('assets/template/landing/assets/vendor/swiper/swiper-bundle.min.css')?>" rel="stylesheet">
 
   <!-- Main CSS File -->
-  <link href="<? base_url('assets/template/landing/')?>assets/css/main.css" rel="stylesheet">
+  <link href="<? base_url('assets/template/landing/assets/css/main.css')?>" rel="stylesheet">
 
   <!-- =======================================================
   * Template Name: MyResume
@@ -798,19 +798,19 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/php-email-form/validate.js"></script>
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/aos/aos.js"></script>
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/typed.js/typed.umd.js"></script>
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="<? base_url('assets/template/landing/')?>assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/php-email-form/validate.js')?>"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/aos/aos.js')?>"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/typed.js/typed.umd.js')?>"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/purecounter/purecounter_vanilla.js')?>"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/waypoints/noframework.waypoints.js')?>"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/glightbox/js/glightbox.min.js')?>"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/imagesloaded/imagesloaded.pkgd.min.j')?>s"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/isotope-layout/isotope.pkgd.min.js')?>"></script>
+  <script src="<? base_url('assets/template/landing/assets/vendor/swiper/swiper-bundle.min.js')?>"></script>
 
   <!-- Main JS File -->
-  <script src="<? base_url('assets/template/landing/')?>assets/js/main.js"></script>
+  <script src="<? base_url('assets/template/landing/assets/js/main.js'?>"></script>
 
 </body>
 
