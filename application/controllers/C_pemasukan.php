ae4ad5cf9b97a1142c2d4e710f51807e061310f5
sukmaaji11 <sukmaaji11@gmail.com>
2025-05-04 22:40:36 +0700

    Fix bug pemasukan debug

--- application/controllers/C_pemasukan.php
+++ application/controllers/C_pemasukan.php
@@ -81,6 +81,8 @@ class C_pemasukan extends CI_Controller
             'updated_at' => date('Y-m-d H:i:s'),
             'pemasukan_status'=> "Approved"
         ];
+        var_dump($data);
+        die;
 
         $this->M_pemasukan->add($data);
     }   
