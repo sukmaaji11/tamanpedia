ccd7909ffbc297cbc7b8ef5f0eb1cf98b2741278
sukmaaji11 <sukmaaji11@gmail.com>
2025-05-04 23:01:16 +0700

    Testing lagi

--- application/views/role2/pemasukan.php
+++ application/views/role2/pemasukan.php
@@ -168,7 +168,7 @@
         
         //BTN ADD Pemasukan
         $('#btn-add-pemasukan').on('click', function() {
-            var pemasukan_kategori = $('input[name=pemasukan_kategori]').val();
+            var pemasukan_kategori = $('select[name=pemasukan_kategori]').val();
             var pemasukan_tgl =  $('input[name=pemasukan_tgl]').val();
             var pemasukan_sumber = $('input[name=pemasukan_sumber]').val();
             var total = $('input[name=pemasukan_total]').val();
